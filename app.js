25424dcf80c227e8026a85f4c030de274e365e3d
WHL99 <xjv298347@gmail.com>
2022-11-25 18:25:20 +0100

    Run client build

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ require("./config")(app);
 const checkedListRoutes = require("./routes/index.routes");
 app.use("/api/checked-list", checkedListRoutes);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+});
+
 require("./error-handling")(app);
 
 module.exports = app;
