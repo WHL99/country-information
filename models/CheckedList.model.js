7c48bb7070b18c1fe178f8dffda04c7f3cec08fb
WHL99 <xjv298347@gmail.com>
2022-11-26 16:45:48 +0100

    Add timestampes in model

--- models/CheckedList.model.js
+++ models/CheckedList.model.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-const checkedListSchema = new Schema({
+const checkedListSchema = new Schema(
+{
   countryName: String,
   counrtyAlpha3Code: String
+},
+{
+  timestamps: true
 });
+
 
 const CheckedList = model('CheckedList', checkedListSchema);
 module.exports = CheckedList;
